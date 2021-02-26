a13f73f6d88c9c706ea220b2564c7c77c03cd3d9
Sven <svo@rother-oss.com>
2021-02-26 08:19:21 +0100

    Add.

--- Kernel/System/CustomerAuth/OneTimeAuthLink.pm
+++ Kernel/System/CustomerAuth/OneTimeAuthLink.pm
@@ -84,10 +84,7 @@ sub Auth {
     }
 
     $Self->{DBObject}->Prepare(
-        SQL => "
-            SELECT Used,User,TicketNumber FROM $Self->{Table} WHERE
-            Token = ?
-            ",
+        SQL => "SELECT Used,User,TicketNumber FROM $Self->{Table} WHERE Token = ?",
         Bind => [ \$Param{OTAToken} ],
     );
 
@@ -132,7 +129,7 @@ sub Auth {
                 Message  => "CustomerUser '$User' tried to log in with an old token. No active token left - sending a new one via mail."
             );
 
-            $Self->GenerateToken(
+            $Self->SendNewToken(
                 User         => $User,
                 TicketNumber => $TicketNumber,
             );
@@ -154,7 +151,6 @@ sub Auth {
     }
 
     return;
-
 }
 
 sub GenerateToken {
@@ -162,7 +158,7 @@ sub GenerateToken {
 
     # check needed stuff
     for my $Needed ( qw/User TicketNumber/ ) {
-        if ( !$Needed ) {
+        if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
@@ -214,7 +210,6 @@ sub GenerateToken {
     );
     
     return $RandomString;
-
 }
 
 sub DeactivateTicketTokens {
@@ -222,7 +217,7 @@ sub DeactivateTicketTokens {
 
     # check needed stuff
     for my $Needed ( qw/TicketNumber/ ) {
-        if ( !$Needed ) {
+        if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
@@ -237,7 +232,6 @@ sub DeactivateTicketTokens {
     );
 
     return 1;
-
 }
 
 sub DeleteTicketTokens {
@@ -245,7 +239,7 @@ sub DeleteTicketTokens {
 
     # check needed stuff
     for my $Needed ( qw/TicketNumber/ ) {
-        if ( !$Needed ) {
+        if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
@@ -260,7 +254,154 @@ sub DeleteTicketTokens {
     );
 
     return 1;
+}
 
+sub TicketLink {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    # check needed stuff
+    for my $Needed ( qw/TicketNumber User/ ) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!"
+            );
+
+            return $ConfigObject->Get('OneTimeAuth::CustomerErrorMessage') // 'Link could not be generated, please contact us directly!';
+        }
+    }
+
+    my $TicketLink = $ConfigObject->Get('HttpType') . '://' . $ConfigObject->Get('FQDN') . '/otobo/customer.pl';
+    $TicketLink .= "?Action=CustomerTicketZoom;TicketNumber=$Param{TicketNumber}";
+
+    # check if the user already has a passsword - then no direct link is needed
+    my %User = $Kernel::OM->Get('Kernel::System::CustomerUser')->CustomerUserDataGet(
+        User => $Param{User},
+    );
+
+    unless ( $User{UserPassword} ) {
+        # generate token
+        my $Token = $Self->GenerateToken(
+            %Param,
+        );
+
+        if ( !$Token ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Error in token generation!"
+            );
+
+            return $ConfigObject->Get('OneTimeAuth::CustomerErrorMessage') // 'Link could not be generated, please contact us directly!';
+        }
+
+        $TicketLink .= ";OTAToken=$Token";
+    }
+
+    return $TicketLink;
+}
+
+sub DeactivateClosedTickets {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    $Self->{DBObject}->Prepare(
+        SQL => "SELECT DISTINCT(ticket_number) FROM $Self->{Table}",
+    );
+
+    # get the compare time if a delay is given
+    my $Delay       = $Kernel::OM->Get('Kernel::Config')->Get('OneTimeAuth::AccessDaysAfterClose') || 0;
+    my $StillOKTime = $Kernel::OM->Create('Kernel::System::DateTime')->ToEpoch() - 24*60*60*$Delay;
+
+    TICKET:
+    while ( my @Row = $DBObject->FetchrowArray() ) {
+        my $TicketID = $TicketObject->TicketIDLookup(
+            TicketNumber => $Row[0],
+        );
+
+        my %Ticket = $TicketObject->TicketGet(
+            TicketID => $TicketID,
+        );
+
+        # remove old tokens
+        if ( $Ticket{StateType} eq 'closed' || $Ticket{StateType} eq 'removed' || $Ticket{StateType} eq 'merged' ) {
+            if ( $Delay ) {
+                my $LastChangedTime = $Kernel::OM->Create(
+                    'Kernel::System::DateTime',
+                    ObjectParams => {
+                        String   => $Ticket{Changed},
+                    }
+                )->ToEpoch();
+
+                next TICKET if $LastChangedTime > $StillOKTime;
+            }
+
+            $Self->DeleteTicketTokens(
+                TicketNumber => $Ticket{TicketNumber},
+            );
+        }
+    }
+
+    return 1;
+}
+
+sub SendNewToken {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed ( qw/TicketNumber User/ ) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!"
+            );
+
+            return $ConfigObject->Get('OneTimeAuth::CustomerErrorMessage') // 'Link could not be generated, please contact us directly!';
+        }
+    }
+
+    my $ConfigObject       = $Kernel::OM->Get('Kernel::Config');
+    my $CustomerUserObject = $Kernel::OM->Get('Kernel::System::CustomerUser');
+    my $TicketObject       = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    my $TicketID = $TicketObject->TicketIDLookup(
+        TicketNumber => $Param{TicketNumber},
+    );
+    my %Ticket = $TicketObject->TicketGet(
+        TicketID => $TicketID,
+    );
+    my %User = $Kernel::OM->Get('Kernel::System::CustomerUser')->CustomerUserDataGet(
+        User => $Param{User},
+    );
+
+    my $TemplateGenerator = $Kernel::OM->Get('Kernel::System::TemplateGenerator');
+    my $Sender            = $TemplateGenerator->Sender(
+        QueueID => $Ticket{QueueID},
+        UserID  => 1,
+    );
+
+    my $TicketLink = $Self->TicketLink( %Param );
+
+    my $EmailObject = $Kernel::OM->Get('Kernel::System::Email');
+    my $Sent = $SendObject->Send(
+        From     => $Sender,
+        To       => $User{UserEmail},
+        Subject  => 'New TicketLink',
+        Charset  => 'iso-8859-15',
+        MimeType => 'text/html',
+        Body     => "Hello hello,<br/>please click sis link: $TicketLink",
+#        InReplyTo     => '<somemessageid-2@example.com>',
+#        References    => '<somemessageid-1@example.com> <somemessageid-2@example.com>',
+#        Loop          => 1, # not required, removes smtp from
+#        CustomHeaders => {
+#            X-OTOBO-MyHeader => 'Some Value',
+#        },
+    );
+
+    return 1;
 }
 
 1;
